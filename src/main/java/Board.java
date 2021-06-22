56a212915e32e77f6c3656c1969f14e52bb43d7a
Heierson <robert.heierson@gmail.com>
2021-06-22 16:04:11 +0200

    GetterAndSetterAdded

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -5,4 +5,12 @@ public class Board {
     public Board(int xWidth, int yHeight) {
         pieces = new String[xWidth][yHeight];
     }
+
+    public String[][] getPieces() {
+        return pieces;
+    }
+
+    public void setPieces(String[][] pieces) {
+        this.pieces = pieces;
+    }
 }
