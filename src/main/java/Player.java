6a479f045d1cabefe2d2c474837fed3e9455000f
Shler Moulodi <shlermoulodi@gmail.com>
2021-06-22 16:03:05 +0200

    Created Pieces abstract class as well as Player class

---
+++ src/main/java/Player.java
@@ -0,0 +1,18 @@
+public class Player extends Piece{
+    private char playerIcon;
+    static int playernumer = 0;
+/*
+    public Player(){
+        playernumer++;
+    }*/
+
+    public Player(int xPosition, int yPosition) {
+        super.setxCoordinate(xPosition);
+        super.setCoordinate(yPosition);
+        this.playerIcon = 'P';
+    }
+
+    char displayPiece(){
+        return playerIcon;
+    }
+}
