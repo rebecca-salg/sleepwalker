3b03c4a1a3b39e7cac057be26de2c5656b711418
lilja-johan <jplilja93@gmail.com>
2021-06-22 15:47:53 +0200

    Created GameLoop in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,31 @@
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Luddeludde");
+    public static void main(String[] args) throws Exception {
+
+        Game game = new Game();
+        game.initiateGame();
+
+        KeyStroke keyStroke;
+        while (true) {
+            keyStroke = null;
+
+            do {
+                Thread.sleep (200);
+                keyStroke = game.terminal.pollInput();
+            } while (keyStroke == null);
+
+            KeyType type = keyStroke.getKeyType();
+            Character c = keyStroke.getCharacter();
+
+            switch (type) {
+                case ArrowUp -> game.movePlayer(KeyType.ArrowUp);
+                case ArrowDown -> game.movePlayer(KeyType.ArrowDown);
+                case ArrowRight -> game.movePlayer(KeyType.ArrowRight);
+                case ArrowLeft -> game.movePlayer(KeyType.ArrowLeft);
+            }
+            game.terminal.flush();
+        }
     }
 }
