b4cf3e2629d1b139c9dd510f59cbe4ed9e23d559
lilja-johan <jplilja93@gmail.com>
2021-06-23 12:38:14 +0200

    finished the starter level

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -9,7 +9,7 @@ public class Game {
     private Terminal terminal;
 
     public Game() throws Exception {
-        this.board = new Board(60, 24);
+        this.board = new Board(80, 24);
         this.terminal = initiateTerminal();
     }
 
@@ -44,12 +44,37 @@ public class Game {
         }
     }
     public void createLevel() throws Exception {
-        drawShape(3,5,5,1);
-        drawShape(3,21,5,1);
-        drawShape(50,2,1,5);
-        drawShape(50,21,1,5);
 
+        drawShape(2,12,0,0); //playerposition
+        drawShape(3,1,5,0);
+        drawShape(3,7,1,5);
+
+        drawShape(45,8,9,0);
+        drawShape(50,7,1,5);
+
+        drawShape(3,20,5,0);
+
+        drawShape(30,20,20,2);
+
+        drawShape(77,15,1,8);
+        drawShape(66,20,11,0);
+
+        drawShape(67,11,9,0);
+        drawShape(67,3,9,0);
+
+        drawShape(65,7,1,0);
+
+        drawShape(62,16,3,0);
+
+        drawShape(3,19,1,5);
+
+        drawShape(15,7,1,3);
+
+        drawShape(13,17,5,0);
+
+        drawShape(60,20,1,0);
     }
+
      public void drawShape(int xStart, int yStart, int length, int height) throws Exception {
         for (int i = yStart - height; i <= yStart; i++) {
             for (int j = xStart; j <= xStart + length; j++){
