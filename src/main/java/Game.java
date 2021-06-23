6aea44fa153fd11509002297dda1c6f4df432809
rebecca-salg <rebeccasalg@hotmail.com>
2021-06-23 11:07:38 +0200

    displayPieces

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,10 +2,11 @@ import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
+import java.io.IOException;
+
 public class Game {
     private Board board;
     private Terminal terminal;
-
 
     public Game() throws Exception {
         this.board = new Board(24, 60);
@@ -25,6 +26,12 @@ public class Game {
         Terminal terminal = terminalFactory.createTerminal();
         terminal.setCursorVisible(false);
         return terminal;
+    }
+
+    public void displayPieces (Piece piece) throws IOException {
+        terminal.setCursorPosition(piece.getxCoordinate(), piece.getCoordinate());
+        terminal.putCharacter(piece.displayPiece());
+        terminal.flush();
     }
 
     public void movePlayer (KeyType keyInput) {
